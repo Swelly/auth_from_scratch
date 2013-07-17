a974440782fb6626a329a3331385f126f2b33362
Wesley Carr <surfswell17@gmail.com>
2013-07-17 10:49:47 -0400

    Form for user entry. Installed bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
+  attr_accessor :password
   attr_accessible :email, :password_hash, :password_salt
+
+    validates :password, :confirmation => true
+    validates :password, :presence => true, :on => :create
+
+    validates :email, :presence => true
+    validates :email, :uniqueness => true
 end
